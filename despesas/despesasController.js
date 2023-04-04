ed05e4c80206bc0d3174132d294e56ed2ee1947b
João Elias Maccari <joaomaccari@hotmail.com>
2023-04-04 11:41:24 -0300

    att
    vendas painel

---
+++ despesas/despesasController.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+
+const connection = require('../database/connection');
+const despesa = require('../models/despesaModel');
+const Despesa = require('../models/despesaModel');
+
+
+router.get("/despesas" ,(req, res) =>{
+    Venda.findAll()
+        .then(despesas => res.json(despesas))
+        
+        .catch(err =>{
+            res.sendStatus(500);
+            console.log(err);
+        });    
+});
+
+router.get('/despesa/:id', (req, res)=>{
+    let id= req.params.id
+
+    if(isNaN(id)){
+        res.sendStatus(400);
+    }
+    else{
+        let {id} = req.params
+        Despesa.findOne({where : {id: id}})
+        .then(despesa =>{
+            if(despesa == null){
+                res.sendStatus(404);
+            }
+            else{
+                res.json(venda)
+            }
+        })
+        .catch(err =>{
+            console.log(err);
+            res.sendStatus(500)
+        })
+    }
+})
---
+++ models/despesaModel.js
@@ -0,0 +1,31 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/connection");
+
+const Despesa = connection.define("despesas", {
+    quantidade:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    produto: {
+        type:Sequelize.STRING(155),
+        allowNull:false
+    },
+    valor:{
+        type:Sequelize.DECIMAL(10,2),
+        allowNull:false
+    },
+    createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW'),
+    },updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW'),
+    }
+
+});
+
+//Despesa.sync({force:true})
+
+module.exports = Despesa;
